3b558103a448b52ab5e963cb8526fe95b9935ae2
Juan Carlos Prieto <JuanC.Prieto.Silos@gmail.com>
2026-01-04 22:16:20 +0100

    Actualización a la versión 2.0.2: implementación dinámica de la versión en Constantes y configuración del manifest en el pom.xml.

--- src/main/java/es/jklabs/utilidades/Constantes.java
+++ src/main/java/es/jklabs/utilidades/Constantes.java
@@ -3,9 +3,14 @@ package es.jklabs.utilidades;
 public class Constantes {
 
     public static final String NOMBRE_APP = "M.C.D. Java";
-    public static final String VERSION = "2.0.1";
+    public static final String VERSION = resolveVersion();
 
     private Constantes() {
 
+    }
+
+    private static String resolveVersion() {
+        String version = Constantes.class.getPackage().getImplementationVersion();
+        return version != null ? version : "dev";
     }
 }
